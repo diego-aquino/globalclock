bf1621649d84af8c67c9ea87fd44a31eec9f9712
Diego Aquino <diego.cruz001@gmail.com>
2020-12-24 17:27:36 -0300

    Add initial theme and global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react';
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import theme from 'styles/theme';
+import GlobalStyle from 'styles/global';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,3 @@
+const theme = { colors: {} };
+
+export default theme;
