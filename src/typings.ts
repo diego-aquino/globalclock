8016324c9082ea564d97a782b3abcbdbabe6323a
Diego Aquino <diego.cruz001@gmail.com>
2020-12-30 19:02:17 -0300

    Use Here's REST API for geolocation services

--- src/typings.ts
+++ src/typings.ts
@@ -1,5 +1,4 @@
 import { SVGAttributes } from 'react';
-import { HereService } from 'services/here/types';
 
 export type TimeOfDay = 'morning' | 'afternoon' | 'evening' | 'night';
 
@@ -33,10 +32,5 @@ export type TimeZone = {
 export type Action<T, V = void> = V extends void
   ? { type: T }
   : { type: T } & V;
-
-export type FinalWindow = Window &
-  typeof globalThis & {
-    H: { service: HereService };
-  };
 
 export type SVGElementProps = SVGAttributes<SVGElement>;
