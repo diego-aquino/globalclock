cff379c3ea15d50943bd862d6d3842893ee37657
Diego Aquino <diego.cruz001@gmail.com>
2021-01-05 15:07:30 -0300

    Implement RichSelect component

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,5 +1,6 @@
 import Layout from './Layout';
 import Input from './Input';
 import SmartInput from './SmartInput';
+import RichSelect from './RichSelect';
 
-export { Layout, Input, SmartInput };
+export { Layout, Input, SmartInput, RichSelect };
