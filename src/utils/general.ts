99c85075d00a777fbd8d41cd948cc5884f89ac20
Diego Aquino <diego.cruz001@gmail.com>
2021-01-07 20:35:59 -0300

    Add utility to turn hex color codes into RGBA

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,3 +1,27 @@
 export function getTypedObjectKeys<T>(object: T): (keyof T)[] {
   return Object.keys(object) as (keyof T)[];
 }
+
+export function toRBGA(hexColor: string, opacity = 1): string {
+  const hexCode = hexColor.slice(1, 7);
+
+  const hexGroupedCodes: string[] = [];
+
+  Array.from(hexCode).forEach((hexNumber, index) => {
+    if (index % 2 === 0) {
+      hexGroupedCodes.push(hexNumber);
+      return;
+    }
+
+    const indexToBeAdded = Math.floor(index / 2);
+    hexGroupedCodes[indexToBeAdded] += hexNumber;
+  });
+
+  const [redRGB, greenRGB, blueRGB] = hexGroupedCodes.map((code) =>
+    parseInt(code, 16),
+  );
+
+  const colorAsRGBA = `rgba(${redRGB}, ${greenRGB}, ${blueRGB}, ${opacity})`;
+
+  return colorAsRGBA;
+}
