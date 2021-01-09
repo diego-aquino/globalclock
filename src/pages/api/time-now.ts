c4b85209a151d48be762041c675ece1eaa822ef8
Diego Aquino <diego.cruz001@gmail.com>
2021-01-09 19:06:59 -0300

    Create types for API response data

--- src/pages/api/time-now.ts
+++ src/pages/api/time-now.ts
@@ -1,9 +1,14 @@
 import { APIRequestHandler } from 'typings';
 
+export interface ResponseData {
+  time: string;
+}
+
 const timeNowHandler: APIRequestHandler = async (_, response) => {
   const currentISOTimeString = new Date().toISOString();
+  const responseData: ResponseData = { time: currentISOTimeString };
 
-  return response.status(200).json({ time: currentISOTimeString });
+  return response.status(200).json(responseData);
 };
 
 export default timeNowHandler;
