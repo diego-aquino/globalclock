0eab8b86d4cbd1027788aa1f334d225034c97bc9
Diego Aquino <diego.cruz001@gmail.com>
2021-01-11 19:42:05 -0300

    Increase value of font size `tiny`

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -23,7 +23,7 @@ const theme = {
       medium: '1.7rem',
       normal: '1.6rem',
       small: '1.3rem',
-      tiny: '1.1rem',
+      tiny: '1.2rem',
     },
     transitionDuration: '0.15s',
     boxShadowBase: `0px 4px 4px ${toRGBA(baseColors.primary, 0.25)}`,
