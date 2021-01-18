69b39b86ab4b6ff35382e723c2aef67d189ac272
Diego Aquino <diego.cruz001@gmail.com>
2021-01-18 20:37:04 -0300

    Handle results of requesting the user location

---
+++ src/utils/message/messageContents.tsx
@@ -0,0 +1,12 @@
+const messageContents = {
+  'userPosition.geolocationNotSupported': (
+    <>
+      Sorry. Geolocation is not supported by your browser.
+      <br />
+      Try updating it, or search the city you&apos;re currently in by its name.
+    </>
+  ),
+  'userPosition.geolocationRequestFailed': 'Sorry. Could not locate you.',
+};
+
+export default messageContents;
