a3e6352b0d750aee3aef01a0918942c3d0991af3
Diego Aquino <diego.cruz001@gmail.com>
2021-01-21 09:53:59 -0300

    Fix issues with body height on mobile devices

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,23 +3,28 @@ import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
+import { useWindowSize } from 'hooks';
 import { LocationContextProvider } from 'contexts/location';
 import theme from 'styles/theme';
 import GlobalStyle from 'styles/global';
 
-const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const windowSize = useWindowSize();
 
-    <ThemeProvider theme={theme}>
-      <LocationContextProvider>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </LocationContextProvider>
-    </ThemeProvider>
-  </>
-);
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <LocationContextProvider>
+          <GlobalStyle minBodyHeight={windowSize.height} />
+          <Component {...pageProps} />
+        </LocationContextProvider>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
