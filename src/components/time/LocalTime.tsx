fef2a76e8151bb25cf9441e492f30a3f8237123b
Diego Aquino <diego.cruz001@gmail.com>
2021-01-21 17:18:06 -0300

    Show a tooltip with the time zone name on hover

--- src/components/time/LocalTime.tsx
+++ src/components/time/LocalTime.tsx
@@ -2,20 +2,31 @@ import { FC } from 'react';
 
 import { DateTime } from 'luxon';
 
+import { TimeZone } from 'typings';
+import { WithTooltip } from 'components/common';
 import {
   Container,
   StyledTime,
-  TimeZone,
+  TimeZoneLabel,
 } from 'styles/components/time/LocalTime';
 
 interface Props {
   dateTime: DateTime;
+  timeZone: TimeZone;
 }
 
-const LocalTime: FC<Props> = ({ dateTime }) => (
+const LocalTime: FC<Props> = ({ dateTime, timeZone }) => (
   <Container>
     <StyledTime initialDateTime={dateTime} />
-    <TimeZone>{dateTime.offsetNameShort}</TimeZone>
+    <WithTooltip
+      id="timeZoneLabel"
+      place="right"
+      overridePosition={(position) => position}
+      render={(elementProps) => (
+        <TimeZoneLabel {...elementProps}>{timeZone.nameShort}</TimeZoneLabel>
+      )}
+      tooltip={timeZone.nameLong}
+    />
   </Container>
 );
 
